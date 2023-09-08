9873eee2a1e19968b06e0e57fb1745eb6797fafb
Kalp Patel <kmp6271@psu.edu>
2023-09-08 09:13:28 -0400

    update readme, add delete data endpoint

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nessie_app
 http://api.nessieisreal.com/
+
+# How to Run
+1. Please make sure you are using Python 3.8 as it might cause compatibility issues with package versions:  
+2. Install the required packages:  
+`pip install -r requirements.txt` (you might have to do `python3.8 -m pip install -r requirements.txt`)  
+3. Create a file called `.env` in the main project directory and add your API_KEY. The `.env` file should look like:  
+`API_KEY=<your_api_key>`  
+4. Start the Flask app!   
+`python app.py` (you might have to do `python3.8 app.py`)
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, render_template, request
+from flask import Flask, render_template, request
 from flask_cors import CORS
 from decouple import config
 
@@ -10,32 +10,31 @@ CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def name():
-    currentCustomer = request.args.get('customer_name')
-    return render_template('home.html', current_tab='Home', currentCustomer=currentCustomer)
+    return render_template('home.html', current_tab='Home', api_key=API_KEY)
 
 @app.route('/atms')
 def atms():
-    currentCustomer = request.args.get('customer_name')
-    return render_template('atms.html', current_tab='ATMs', currentCustomer=currentCustomer, api_key=API_KEY)
+    return render_template('atms.html', current_tab='ATMs', api_key=API_KEY)
 
 @app.route('/customers')
 def customers():
-    currentCustomer = request.args.get('customer_name')
-    return render_template('customers.html', current_tab='Customers', currentCustomer=currentCustomer, api_key=API_KEY)
+    return render_template('customers.html', current_tab='Customers', api_key=API_KEY)
 
 @app.route('/accounts')
 def accounts():
-    currentCustomer = request.args.get('customer_name')
-    return render_template('accounts.html', current_tab='Accounts', currentCustomer=currentCustomer, api_key=API_KEY)
+    return render_template('accounts.html', current_tab='Accounts', api_key=API_KEY)
 
 @app.route('/activity')
 def purchases():
-    currentCustomer = request.args.get('customer_name')
-    return render_template('activity.html', current_tab='Actiity', currentCustomer=currentCustomer, api_key=API_KEY)
+    return render_template('activity.html', current_tab='Actiity', api_key=API_KEY)
 
 @app.route('/enterprise')
 def enterprise():
     return render_template('enterprise.html', current_tab='Enterprise', api_key=API_KEY)
+
+@app.route('/delete')
+def delete():
+    return render_template('delete.html', current_tab='Delete', api_key=API_KEY)
 
 if __name__ == '__main__':
     app.run(debug=True)
